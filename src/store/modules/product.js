04ad1acb0064d9cf07ddb42ee5deab0323d294d6
yinhaogt <1216958224@qq.com>
2019-10-27 21:36:07 +0800

    [殷浩]提交初始代码

--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -1,73 +1,115 @@
 import request from '@/utils/request'
+import { post, post_array } from '@/utils/request'
 
 export default {
-  namespaced:true,
-  state:{
-    products:[],
-    visible:false,
-    title:"添加产品信息"
-  },
-  getters:{
-    productSize(state){
-      return state.products.length;
+  namespaced: true,
+  state: {
+    products: {
+      list: []
     },
-    orderProduct:(state)=>{
-      return function(flag){
-        state.products.sort((a,b)=>{
-          if(a[flag] > b[flag]){
-            return -1;
+    params: {
+      page: 0,
+      pageSize: 6,
+      name: '',
+      status: ''
+    },
+    visible: false,
+    title: '添加分类信息',
+    categoryIds: []
+  },
+  getters: {
+    productSize(state) {
+      return state.products.length
+    },
+    orderProduct: (state) => {
+      return function(flag) {
+        state.products.sort((a, b) => {
+          if (a[flag] > b[flag]) {
+            return -1
           } else {
-            return 1;
+            return 1
           }
         })
-        return state.products;
+        return state.products
       }
     }
   },
-  mutations:{
-    showModal(state){
-      state.visible = true;
+  mutations: {
+    showModal(state) {
+      state.visible = true
     },
-    closeModal(state){
-      state.visible = false;
+    jiangxu: (state) => {
+      state.products.list.sort((a, b) => {
+        if (a.price > b.price) {
+          return -1
+        } else {
+          return 1
+        }
+      })
+      // return state.products;
     },
-    refreshProducts(state,products){
-      state.products = products;
+    shengxu: (state) => {
+      state.products.list.sort((a, b) => {
+        if (a.price > b.price) {
+          return 1
+        } else {
+          return -1
+        }
+      })
+      // return state.products;
     },
-    setTitle(state,title){
-      state.title = title;
+    closeModal(state) {
+      state.visible = false
+    },
+    refreshCategories(state, products) {
+      state.products = products
+      state.categoryIds = []
+      products.list.map((item) => {
+        if (item.categoryId && state.categoryIds.indexOf(item.categoryId) == -1) {
+          state.categoryIds.push(item.categoryId)
+        }
+      })
+    },
+    loadCategoryId(state, products) {
+      state.products.list = products
+    },
+    setTitle(state, title) {
+      state.title = title
     }
   },
-  actions:{
-    async batchDeleteProduct(context,ids){
+  actions: {
+    async batchDeleteProduct({ dispatch, rootState }, ids) {
       // 1. 批量删除
-      let response = await request.post("/product/batchDelete",{ids})
+      const response = await post_array('/product/batchDelete', { ids })
       // 2. 分发
-      context.dispatch("findAllProducts");
+      dispatch('loadPages', rootState.product.params)
       // 3. 返回结果
-      return response;
+      return response
     },
-    async deleteProductById(context,id){
-      let response = await request.get("/product/deleteById?id="+id);
-      context.dispatch("findAllProducts");
-      return response;
+    async deleteProductById({ dispatch, rootState }, id) {
+      const response = await request.get('/product/deleteById?id=' + id)
+      dispatch('loadPages', rootState.product.params)
+      return response
     },
-    async findAllProducts(context){
-      // 1. ajax查询
-      let response = await request.get("/product/findAll");
-      // 2. 将查询结果更新到state中
-      context.commit("refreshProducts",response.data);
-    },
-    // payload 顾客信息
-    async saveOrUpdateProduct({commit,dispatch},payload){
+
+    async saveOrUpdateProduct({ commit, dispatch, rootState }, payload) {
       // 1. 保存或更新
-      let response = await request.post("/product/saveOrUpdate",payload)
+      const response = await post('/product/saveOrUpdate', payload)
       // 2. 刷新页面
-      dispatch("findAllProducts");
+      // console.log(rootState)
+      dispatch('loadPages', rootState.product.params)
       // 3. 关闭模态框
-      commit("closeModal");
+      commit('closeModal')
       // 4. 提示
-      return response;
+      return response
+    },
+    async loadPages(context, payload) {
+      const response = await post('/product/query', payload)
+      context.commit('refreshCategories', response.data)
+    },
+    async searchByCategoryId(context, payload) {
+      const response = await request.get('/product/findByCategoryId?id=' + payload)
+      context.commit('loadCategoryId', response.data)
     }
   }
 }
